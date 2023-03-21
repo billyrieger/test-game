743756dfb3d31052825c3e5d43b4133b113ea90a
Billy Rieger <wrieger@protonmail.com>
2023-03-21 00:08:46 -0400

    Create a basic bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("goat_circle.png"),
+        ..default()
+    });
 }
