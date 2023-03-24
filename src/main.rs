59ccfc16f65de0350bbf4dd03191b60cf1aa3006
Billy Rieger <wrieger@protonmail.com>
2023-03-24 11:06:30 -0400

    Add simple tweening animation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_tweening::*;
 
 const WIDTH: f32 = 800.;
 const HEIGHT: f32 = 600.;
@@ -12,14 +13,28 @@ fn main() {
             }),
             ..default()
         }))
+        .add_plugin(TweeningPlugin)
         .add_startup_system(setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tween = Tween::new(
+        EaseFunction::QuadraticInOut,
+        std::time::Duration::from_secs(1),
+        lens::TransformPositionLens {
+            start: Vec3::new(-100., 100., 0.),
+            end: Vec3::new(100., -100., 0.),
+        },
+    )
+    .with_repeat_count(RepeatCount::Infinite)
+    .with_repeat_strategy(RepeatStrategy::MirroredRepeat);
+
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("goat_circle.png"),
-        ..default()
-    });
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("goat_circle.png"),
+            ..default()
+        })
+        .insert(Animator::new(tween));
 }
